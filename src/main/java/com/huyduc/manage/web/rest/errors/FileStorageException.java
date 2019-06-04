d8fda3fa782154a69a415554b61f08ac433c52da
dao huy duc <ducanime1@gmail.com>
2019-06-05 01:37:32 +0700

    new

--- src/main/java/com/huyduc/manage/web/rest/errors/FileStorageException.java
+++
@@ -1,8 +0,0 @@
-package com.huyduc.manage.web.rest.errors;
-
-public class FileStorageException extends BadRequestAlertException {
-
-    public FileStorageException() {
-        super(ErrorConstants.FILE_UPLOAD_FAILURE, "Upload hình ảnh thất bại!", "userManagement", "file");
-    }
-}
--- src/main/java/com/huyduc/manage/web/rest/errors/YearAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.huyduc.manage.web.rest.errors;
-
-public class YearAlreadyExistException extends BadRequestAlertException {
-    public YearAlreadyExistException() {
-        super(ErrorConstants.YEAR_ALREADY_EXISTS_TYPE, "Năm học đã tồn tại!", "yearManagement", "yearexists");
-    }
-}
